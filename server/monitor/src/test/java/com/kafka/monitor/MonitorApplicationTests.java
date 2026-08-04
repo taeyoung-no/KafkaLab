3d6288f37c9e491e6350a207f9974de50b0834b8
taeyoung-no <notaeyoung03@gmail.com>
2026-08-04 09:55:33 +0900

    initial commit

---
+++ server/monitor/src/test/java/com/kafka/monitor/MonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
